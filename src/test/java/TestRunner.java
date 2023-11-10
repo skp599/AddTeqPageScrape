1389540795a721ffd5c57d0c544482d066f20809
Shiv Kumar <97832177+skp599@users.noreply.github.com>
2023-11-10 16:23:28 +0530

    Test runner class updated

--- src/test/java/TestRunner.java
+++ src/test/java/TestRunner.java
@@ -5,7 +5,7 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 
 @CucumberOptions(features = "src/test/resources/features",
-        glue = {"stepdefinitions"},
+
         plugin = {"pretty", "html:target/cucumber-reports", "json:target/cucumber.json"},
         monochrome = true,
         publish = true)
